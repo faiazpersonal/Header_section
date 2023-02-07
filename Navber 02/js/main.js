4f9299e4b4f1483032f6b861b3601f8c4fc78860
faiaz <faiaz.parsonal@gmail.com>
2023-02-07 18:05:33 +0600

    Navigation Bar

---
+++ Navber 02/js/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+	"use strict";
+ $('.navbar-nav li.dropdown').hover(function() {
+    $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
+    }, function() {
+    $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
+    });
+
+    
+    // Search Toggle
+    $("#search_input_box").hide();
+    $("#search").on("click", function () {
+        $("#search_input_box").slideToggle();
+        $("#search_input").focus();
+    });
+    $("#close_search").on("click", function () {
+        $('#search_input_box').slideUp(500);
+    });
+
+    /*==========================
+		javaScript for sticky header
+		============================*/
+			$(".sticky-header").sticky();
+
+ });
